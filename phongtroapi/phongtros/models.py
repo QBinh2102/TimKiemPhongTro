99d88eee3af0d94f103c85d2101da03f1589085b
QBinh2102 <toquocbinh2102@gmail.com>
2025-01-10 11:05:30 +0700

    Cap nhat model

--- phongtroapi/phongtros/models.py
+++ phongtroapi/phongtros/models.py
@@ -1,17 +1,53 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 from django.db.models import Model
+from enum import IntEnum
 
+class VaiTro(IntEnum):
+    QUANTRIVIEN = 1
+    CHUNHATRO = 2
+    NGUOITHUETRO = 3
+
+    @classmethod
+    def choices(cls):
+        return [(key.value, key.name) for key in cls]
 
 class User(AbstractUser):
-    pass
+    SDT = models.SmallIntegerField(null=True)
+    vaiTro = models.IntegerField(choices=VaiTro.choices(),default=VaiTro.QUANTRIVIEN)
 
+class NguoiDung(User):
+    avatar = models.ImageField(upload_to='nguoidungs/%Y/%m')
+    tuongTac = models.ManyToManyField("self")
 
-class Tro(models.Model):
+class BaseModel(models.Model):
+    active = models.BooleanField(default=True)
+    created_date = models.DateTimeField(auto_now_add=True)
+    # updated_date = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        abstract = True
+
+class Tro(BaseModel):
     tenTro = models.CharField(max_length=255)
     anhTro = models.ImageField(upload_to='phongtros/%Y/%m/')
     diaChi = models.CharField(max_length=255)
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    nguoiChoThue = models.ForeignKey(NguoiDung, on_delete=models.CASCADE)
+    gia = models.PositiveIntegerField()
+    soNguoiO = models.SmallIntegerField()
 
     def __str__(self):
         return self.tenTro
+
+class BaiDang(BaseModel):
+    updated_date = models.DateTimeField(auto_now=True)
+    nguoiDangBai = models.ForeignKey(NguoiDung, on_delete=models.CASCADE)
+
+class BaiDangChoThue(BaiDang):
+    troChoThue = models.ForeignKey(Tro, on_delete=models.CASCADE)
+
+class BinhLuan(BaseModel):
+    thongTin = models.CharField(max_length=255)
+    updated_date = models.DateTimeField(auto_now=True)
+    nguoiBinhLuan = models.ForeignKey(NguoiDung, on_delete=models.CASCADE)
+    baiDang = models.ForeignKey(BaiDang,on_delete=models.CASCADE)
