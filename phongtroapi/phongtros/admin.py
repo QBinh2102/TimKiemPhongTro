e13587c16c435f4efd489f0ad136a69b7dd22579
QBinh2102 <toquocbinh2102@gmail.com>
2025-01-11 22:43:18 +0700

    update models

--- phongtroapi/phongtros/admin.py
+++ phongtroapi/phongtros/admin.py
@@ -1,5 +1,7 @@
-from django.contrib import admin
-from phongtros.models import User, Tro, NguoiDung, BaiDang, BaiDangChoThue, City, District, Ward
+from django import forms
+from django.contrib import admin, messages
+from django.core.exceptions import ValidationError
+from phongtros.models import User, Tro, BaiDang, BaiDangChoThue, City, District, Ward, VaiTro, AnhTro, BinhLuan
 from django.utils.html import mark_safe
 
 
@@ -8,51 +10,135 @@ class MyCourseAdmin(admin.AdminSite):
 
 class UserAdmin(admin.ModelAdmin):
     # Chỉ hiển thị trường 'username' và 'password'
-    fields = ('username', 'password','email','first_name', 'last_name','SDT')
-
-    def save_model(self, request, obj, form, change):
-        # Chỉ gán giá trị mặc định khi tạo mới người dùng
-        if not change:  # Nếu đối tượng là mới (chưa lưu)
-            obj.is_superuser = True
-            obj.is_active = True  # Gán mặc định 'is_active' là True
-            obj.is_staff = True  # Gán mặc định 'is_staff' là False
-            # Băm mật khẩu trước khi lưu
-            obj.set_password(form.cleaned_data['password'])
-        super().save_model(request, obj, form, change)  # Gọi phương thức lưu của lớp cha
-
-class NguoiDungAdmin(admin.ModelAdmin):
     fields = ('username', 'password', 'email', 'first_name', 'last_name', 'SDT', 'vaiTro', 'image', 'avatar')
+    list_display = ['username', 'email', 'SDT']
+    search_fields = ['username']
     readonly_fields = ['avatar']
 
     def avatar(self, nguoidung):
-            return mark_safe(f'<img src="/static/{nguoidung.image.name}" width="200" />')
-
+        return mark_safe(f'<img src="/static/{nguoidung.image.name}" width="200" />')
 
     def save_model(self, request, obj, form, change):
         # Chỉ gán giá trị mặc định khi tạo mới người dùng
-        if not change:  # Nếu đối tượng là mới (chưa lưu)
-            obj.is_superuser = False
-            obj.is_active = True  # Gán mặc định 'is_active' là True
-            obj.is_staff = False  # Gán mặc định 'is_staff' là False
-            # Băm mật khẩu trước khi lưu
+        if not change:
+            if obj.vaiTro == VaiTro.QUANTRIVIEN:
+                obj.is_superuser = True
+                obj.is_staff = True
+            else:
+                obj.is_superuser = False
+                obj.is_staff = False
+            obj.is_active = True
             obj.set_password(form.cleaned_data['password'])
         super().save_model(request, obj, form, change)  # Gọi phương thức lưu của lớp cha
 
+class TroForm(forms.ModelForm):
+    class Meta:
+        model = Tro
+        fields = '__all__'
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        # Lọc người dùng có vai trò là CHUNHATRO
+        self.fields['nguoiChoThue'].queryset = User.objects.filter(vaiTro=VaiTro.CHUNHATRO)
+
+
+class CustomAnhTroInline(admin.TabularInline):
+    model = AnhTro
+    extra = 0
+    readonly_fields = ['image']
+
+    def image(self, anhTro):
+        return mark_safe(f'<img src="/static/{anhTro.anh.name}" width="150" />')
+
+    image.short_description = "Ảnh"
+
+    def has_delete_permission(self, request, obj=None):
+        if obj:
+            current_count = AnhTro.objects.filter(tro=obj).count()
+            if current_count <= 3:
+                return False
+        return super().has_delete_permission(request, obj)
+
+    def get_queryset(self, request):
+        # Lọc ảnh chỉ liên quan đến trọ đang chỉnh sửa
+        qs = super().get_queryset(request)
+        tro_id = request.resolver_match.kwargs.get('object_id')
+        if tro_id:
+            return qs.filter(tro__id=tro_id)
+        return qs
+
 class TroAdmin(admin.ModelAdmin):
-    def formfield_for_foreignkey(self, db_field, request, **kwargs):
-        if db_field.name == 'quan':
-            if request.POST.get('thanh_pho'):
-                thanh_pho_id = request.POST.get('thanh_pho')
-                kwargs['queryset'] = District.objects.filter(parent_code_id=thanh_pho_id)
-        return super().formfield_for_foreignkey(db_field, request, **kwargs)
+    form = TroForm
+    inlines = [CustomAnhTroInline]
+    list_display = ['tenTro', 'diaChi', 'phuong', 'quan', 'thanh_pho', 'soNguoiO', 'gia', 'nguoiChoThue', 'active']
+    search_fields = ['tenTro']
+    list_filter = ['gia']
+
+    def has_add_permission(self, request):
+        return False
+
+class CustomBinhLuanInline(admin.TabularInline):
+    model = BinhLuan
+    extra = 0
+    readonly_fields = ['nguoiBinhLuan', 'thongTin']
+
+    def get_queryset(self, request):
+        bl = super().get_queryset(request)
+        baiDang_id = request.resolver_match.kwargs.get('object_id')
+        if baiDang_id:
+            return bl.filter(baiDang__id=baiDang_id)
+        return bl
+
+class BaiDangForm(forms.ModelForm):
+    class Meta:
+        model = BaiDang
+        fields = '__all__'
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields['nguoiDangBai'].queryset = User.objects.filter(vaiTro=VaiTro.NGUOITHUETRO)
+
+class BaiDangAdmin(admin.ModelAdmin):
+    inlines = [CustomBinhLuanInline]
+    list_display = ['tieuDe', 'created_date', 'nguoiDangBai']
+    search_fields = ['tieuDe']
+    list_filter = ['created_date', 'nguoiDangBai']
+    form = BaiDangForm
+
+    def has_add_permission(self, request):
+        return False
+
+    def get_queryset(self, request):
+        # Lọc chỉ các bài đăng thuộc lớp BaiDang, không bao gồm các lớp con
+        return super().get_queryset(request).filter(baidangchothue__isnull=True)
+
+class BaiDangChoThueForm(forms.ModelForm):
+    class Meta:
+        model = BaiDangChoThue
+        fields = '__all__'
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields['nguoiDangBai'].queryset = User.objects.filter(vaiTro=VaiTro.CHUNHATRO)
+
+class BaiDangChoThueAdmin(admin.ModelAdmin):
+    inlines = [CustomBinhLuanInline]
+    list_display = ['tieuDe', 'created_date', 'nguoiDangBai']
+    search_fields = ['tieuDe']
+    list_filter = ['created_date', 'nguoiDangBai']
+    form = BaiDangChoThueForm
+
+    def has_add_permission(self, request):
+        return False
 
 admin_site = MyCourseAdmin()
 
 admin_site.register(User,UserAdmin)
-admin_site.register(NguoiDung,NguoiDungAdmin)
+# admin_site.register(AnhTro)
 admin_site.register(Tro,TroAdmin)
-admin_site.register(BaiDang)
-admin_site.register(BaiDangChoThue)
+admin_site.register(BaiDang,BaiDangAdmin)
+admin_site.register(BaiDangChoThue,BaiDangChoThueAdmin)
+# admin_site.register(BinhLuan)
 admin_site.register(City)
 admin_site.register(District)
 admin_site.register(Ward)
--- phongtroapi/phongtros/models.py
+++ phongtroapi/phongtros/models.py
@@ -5,8 +5,9 @@ from vi_address.models import City, District, Ward
 from enum import IntEnum
 
 class VaiTro(IntEnum):
-    CHUNHATRO = 1
-    NGUOITHUETRO = 2
+    QUANTRIVIEN = 1
+    CHUNHATRO = 2
+    NGUOITHUETRO = 3
 
     @classmethod
     def choices(cls):
@@ -14,13 +15,8 @@ class VaiTro(IntEnum):
 
 class User(AbstractUser):
     SDT = models.CharField(max_length=10)
-
-    class Meta:
-        verbose_name_plural = 'Admin'
-
-class NguoiDung(User):
     image = models.ImageField(upload_to='nguoidungs/%Y/%m')
-    vaiTro = models.IntegerField(choices=VaiTro.choices(), default=VaiTro.NGUOITHUETRO)
+    vaiTro = models.IntegerField(choices=VaiTro.choices(), default=VaiTro.QUANTRIVIEN)
     tuongTac = models.ManyToManyField("self")
 
     class Meta:
@@ -41,13 +37,16 @@ class AnhTro(models.Model):
     def __str__(self):
         return f'Ảnh của {self.tro.tenTro}'
 
+    class Meta:
+        verbose_name_plural = 'Ảnh Trọ'
+
 class Tro(BaseModel):
     tenTro = models.CharField(max_length=255)
     thanh_pho = models.ForeignKey(City, null=True, blank=True, on_delete=models.SET_NULL)
     quan = models.ForeignKey(District, null=True, blank=True, on_delete=models.SET_NULL)
     phuong = models.ForeignKey(Ward, null=True, blank=True, on_delete=models.SET_NULL)
     diaChi = models.CharField(max_length=255)
-    nguoiChoThue = models.ForeignKey(NguoiDung, on_delete=models.CASCADE)
+    nguoiChoThue = models.ForeignKey(User, on_delete=models.CASCADE)
     gia = models.PositiveIntegerField()
     soNguoiO = models.SmallIntegerField()
 
@@ -58,9 +57,13 @@ class Tro(BaseModel):
         verbose_name_plural = 'Trọ'
 
 class BaiDang(BaseModel):
+    tieuDe = models.CharField(max_length=255)
     updated_date = models.DateTimeField(auto_now=True)
-    thongTin = models.CharField(max_length=255)
-    nguoiDangBai = models.ForeignKey(NguoiDung, on_delete=models.CASCADE)
+    thongTin = models.TextField()
+    nguoiDangBai = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.tieuDe
 
     class Meta:
         verbose_name_plural = 'Bài đăng'
@@ -72,7 +75,19 @@ class BaiDangChoThue(BaiDang):
         verbose_name_plural = 'Bài đăng cho thuê'
 
 class BinhLuan(BaseModel):
-    thongTin = models.CharField(max_length=255)
+    thongTin = models.TextField()
     updated_date = models.DateTimeField(auto_now=True)
-    nguoiBinhLuan = models.ForeignKey(NguoiDung, on_delete=models.CASCADE)
+    nguoiBinhLuan = models.ForeignKey(User, on_delete=models.CASCADE)
     baiDang = models.ForeignKey(BaiDang,on_delete=models.CASCADE)
+
+class Chat(BaseModel):
+    nguoiGui = models.ForeignKey(User, on_delete=models.PROTECT, related_name='tin_nhan_gui')
+    nguoiNhan = models.ForeignKey(User, on_delete=models.PROTECT, related_name='tin_nhan_nhan')
+    thoiGianGui = models.DateTimeField(auto_now_add=True)
+    daDoc = models.BooleanField(default=False)
+
+class ChatText(Chat):
+    noiDung = models.TextField()
+
+class ChatAnh(Chat):
+    anh = models.ImageField(upload_to='chats/%Y/%m')
