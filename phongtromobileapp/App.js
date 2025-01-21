00ffaba741fbf32dcbac4df891a2cb5ee38e09dc
HSang1 <huynhsang2004a@gmail.com>
2025-01-21 19:48:49 +0700

    ket noi firebase

--- phongtromobileapp/App.js
+++ phongtromobileapp/App.js
@@ -1,4 +1,32 @@
 import Home from './components/Home/Home';
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+
+import { getDatabase, ref, set } from "firebase/database";
+
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAjAiJfAaclDPjK2B5M9sBUud4phIpb9ow",
+  authDomain: "timkiemphongtro-e5cc6.firebaseapp.com",
+  databaseURL: "https://timkiemphongtro-e5cc6-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "timkiemphongtro-e5cc6",
+  storageBucket: "timkiemphongtro-e5cc6.firebasestorage.app",
+  messagingSenderId: "525846725068",
+  appId: "1:525846725068:web:dbbe97f76dff3af3de5d69",
+  measurementId: "G-XTCXJFP2NY"
+};
+
+
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+
+function guiDuLieu() {
+  const db = getDatabase();
+  set(ref(db, 'Dia chi gui len'), {
+    tenBien: "gia tri gui len"  });
+}
+
 
 export default function App() {
   return (
